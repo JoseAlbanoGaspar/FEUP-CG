b1d1234f213fdd1729a739efd56577cc66d50116
Jose Gaspar <up202008561@edu.fe.up.pt>
2023-04-06 15:53:08 +0100

    scenario 1

---
+++ project/MySphere.js
@@ -0,0 +1,60 @@
+import { CGFobject } from '../lib/CGF.js';
+
+export class MySphere extends CGFobject {
+  constructor(scene, radius, slices, stacks) {
+    super(scene);
+
+    this.radius = radius;
+    this.slices = slices;
+    this.stacks = stacks;
+
+    this.initBuffers();
+  }
+
+  initBuffers() {
+    this.vertices = [];
+    this.indices = [];
+    this.normals = [];
+    this.texCoords = [];
+
+    for (let i = 0; i <= this.stacks; i++) {
+      let theta = i * Math.PI / this.stacks;
+      let sinTheta = Math.sin(theta);
+      let cosTheta = Math.cos(theta);
+
+      for (let j = 0; j <= this.slices; j++) {
+        let phi = j * 2 * Math.PI / this.slices;
+        let sinPhi = Math.sin(phi);
+        let cosPhi = Math.cos(phi);
+
+        let x = cosPhi * sinTheta;
+        let y = cosTheta;
+        let z = sinPhi * sinTheta;
+
+        let u = j / this.slices;
+        let v = i / this.stacks;
+
+        this.vertices.push(this.radius * x, this.radius * y, this.radius * z);
+        this.normals.push(x, y, z);
+        //scenario 2
+        //this.normals.push(-x, -y, -z);
+        this.texCoords.push(u, v);
+      }
+    }
+
+    for (let i = 0; i < this.stacks; i++) {
+      for (let j = 0; j < this.slices; j++) {
+        let a = i * (this.slices + 1) + j;
+        let b = a + this.slices + 1;
+        //scenario 2
+        //this.indices.push(a, b, a + 1);
+        //this.indices.push(b, b + 1, a + 1);
+        this.indices.push(a + 1, b , a);
+        this.indices.push(a + 1, b + 1, b);
+      }
+    }
+
+    this.primitiveType = this.scene.gl.TRIANGLES;
+    this.initGLBuffers();
+  }
+}
